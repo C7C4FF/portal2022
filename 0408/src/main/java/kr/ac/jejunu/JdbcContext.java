614c71cb507269110e3f4114b769129fca055366
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2022-04-08 18:07:04 +0900

    18:00

--- 0408/src/main/java/kr/ac/jejunu/JdbcContext.java
+++ 0408/src/main/java/kr/ac/jejunu/JdbcContext.java
@@ -1,10 +1,7 @@
 package kr.ac.jejunu;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcContext {
     private final DataSource dataSource;
@@ -112,5 +109,53 @@ public class JdbcContext {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void insert(User user, String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement= connection.prepareStatement(
+                    sql
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+
+            for(int i = 0; i< params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+
+            return preparedStatement;
+        };
+        jdbcContextForInsert(user, statementStrategy);
+    }
+
+    public void update(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    sql
+            );
+
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+//            preparedStatement.setObject(1, user.getName());
+//            preparedStatement.setObject(2, user.getPassword());
+//            preparedStatement.setObject(3, user.getId());
+            return preparedStatement;
+        };
+        jdbcContextForUpdate(statementStrategy);
+    }
+
+    public User find(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    sql
+            );
+
+            for(int i = 0; i< params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+
+            return preparedStatement;
+        };
+        return jdbcContextForFind(statementStrategy);
     }
 }
