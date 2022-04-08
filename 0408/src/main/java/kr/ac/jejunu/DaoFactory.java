17ad564a58e4832eec22795000fb20d0ecf7d8dc
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2022-04-08 16:42:27 +0900

    0408 16:50

---
+++ 0408/src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -0,0 +1,39 @@
+package kr.ac.jejunu;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+@Configuration
+public class DaoFactory {
+
+    @Value("${db.drivername}")
+    private String driverClassName; // = "com.mysql.cj.jdbc.Driver";
+    @Value("${db.url}")
+    private String url; // = "jdbc:mysql://localhost:3306/jeju";
+    @Value("${db.username}")
+    private String username; // = "root";
+    @Value("${db.password}")
+    private String password; // = "1234";
+
+
+    @Bean
+    public UserDao userDao() throws ClassNotFoundException {
+        return new UserDao(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(
+                (Class<? extends Driver>) Class.forName(driverClassName));
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
