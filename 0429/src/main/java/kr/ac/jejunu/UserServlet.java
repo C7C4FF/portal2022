24d4d8d1bcd8e214d49b1186eb1e2d0a5ab165e5
C7C4FF <80257041+C7C4FF@users.noreply.github.com>
2022-05-06 18:23:57 +0900

    0506

--- 0429/src/main/java/kr/ac/jejunu/UserServlet.java
+++ 0429/src/main/java/kr/ac/jejunu/UserServlet.java
@@ -1,9 +1,15 @@
 package kr.ac.jejunu;
 
+import lombok.SneakyThrows;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import javax.servlet.*;
 import java.io.IOException;
 
 public class UserServlet extends GenericServlet {
+    private UserDao userDao;
+
     @Override
     public void destroy() {
         System.out.println("********* destroy *********");
@@ -12,14 +18,19 @@ public class UserServlet extends GenericServlet {
 
     @Override
     public void init() throws ServletException {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("kr.ac.jejunu");
+        this.userDao = applicationContext.getBean("userDao", UserDao.class);
         System.out.println("*********  init *********");
         super.init();
     }
 
+    @SneakyThrows
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        Integer id = Integer.parseInt(req.getParameter("id"));
+        User user = userDao.findById(id);
         res.setContentType("text/html;charset=UTF-8");
-        String html = "<html><h1> Hi 김희범 </h1></html>";
+        String html = String.format("<html><h1> Hi %s </h1></html>", user.getName());
         res.getWriter().println(html);
         System.out.println("********* service *********");
     }
